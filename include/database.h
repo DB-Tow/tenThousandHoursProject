a13705c48ec5fca7773f2f1f0a0de390ecb895a4
DB-Tow <dbtowdev@gmail.com>
2025-09-22 14:04:34 -0700

    Initial Database Setup / Linking files

---
+++ include/database.h
@@ -0,0 +1,8 @@
+// database.h TenThousandHoursProject [Created by DBTow]
+
+#ifndef DATABASE_H
+#define DATABASE_H
+
+int db_init(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -3,10 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "database.h"
+
 int main(int argc, char *argv[])
 {
 	printf("Welcome to the TTHP time logging program\n");
 	printf("\n");
+
+	if (db_init() != 0) {
+		fprintf(stderr, "Database initialization failed!\n");
+		return 1;
+	}
+	printf("Database initialized successfully.\n\n");
 
 	int i;
 	char *commands[] = { "log" };
