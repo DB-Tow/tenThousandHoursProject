b217b3cfd859e8fe6b7e118a7afd467bbaf48ee4
DB-Tow <dbtowdev@gmail.com>
2025-09-28 14:38:50 -0700

    Parsing Time String

--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,18 @@
 // parser.c TenThousandHoursProject TTHP [Created by DBTow]
+
+#include <stdio.h>
+
+int parse_time(const char *time_str);
+
+int parse_time(const char *time_str)
+{
+	int hours;
+	int minutes;
+
+	int result = sscanf(time_str, "%dh %dm", &hours, &minutes);
+
+	int total_seconds = (hours * 3600) + (minutes * 60);
+
+	return total_seconds;
+}
+
