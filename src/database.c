a667bdb33c6f4b42f8245bffa048bc02a205c7a3
DB-Tow <dbtowdev@gmail.com>
2025-09-28 19:05:05 -0700

    Updated database schema and initialization

--- src/database.c
+++ src/database.c
@@ -20,25 +20,47 @@ int db_init() {
 		return 1;
 	}
 	
-	char *sql =
+	// Creating main time_entries table
+	const char *sql_time_entries =
 		"CREATE TABLE IF NOT EXISTS time_entries ("
-		"id INTEGER PRIMARY KEY, "
-		"seconds INTEGER, "
-		"category TEXT, "
-		"description TEXT"
+		"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		"seconds INTEGER NOT NULL, "
+		"category TEXT NOT NULL, "
+		"date TEXT NOT NULL DEFAULT (DATE('now'))"
 		");";
 
-	rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+	rc = sqlite3_exec(db, sql_time_entries, 0, 0, &err_msg);
 
 	if (rc != SQLITE_OK) {
-		fprintf(stderr, "SQL error: %s\n", err_msg);
+		fprintf(stderr, "SQL error (time entries table): %s\n", err_msg);
 
 		sqlite3_free(err_msg);
 		sqlite3_close(db);
 
 		return 1;
 	}
-	
+
+	// Creating descriptions table
+	const char *sql_descriptions = 
+		"CREATE TABLE IF NOT EXISTS descriptions ("
+		"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		"time_entry_id INTEGER NOT NULL, "
+		"comment TEXT NOT NULL,"
+		"FOREIGN KEY (time_entry_id) REFERENCES time_entries(id) "
+		"ON DELETE CASCADE"
+		");";
+
+	rc = sqlite3_exec(db, sql_descriptions, 0, 0, &err_msg);
+
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error (descriptions table): %s\n", err_msg);
+
+		sqlite3_free(err_msg);
+		sqlite3_close(db);
+
+		return 1;
+	}
+
 	sqlite3_close(db);
 	return 0;
 }
