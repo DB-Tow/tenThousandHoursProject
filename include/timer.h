f23a4b5f823a7779077e505b761bae79e41826b1
DB-Tow <dbtowdev@gmail.com>
2025-11-17 15:43:45 -0700

    timer state control functions

---
+++ include/timer.h
@@ -0,0 +1,29 @@
+// timer.h tenThousandHoursProject tthp [Created by DBTow]
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <time.h>
+#include <stdbool.h> 
+
+// Path to the timer state file
+#define TIMER_STATE_PATH "data/timer_state"
+
+// Timer state struct
+typedef struct {
+	bool is_running; 		// 1 = running and 0 = not running
+	time_t start_time; 		// A Unix timestamp of when the timer was last started
+	int accumulated_time; 		// The amount of accumulated time counted 
+} TimerState;
+
+// Function declarations
+
+int timer_read_state(TimerState *state);
+
+int timer_write_state(const TimerState *state);
+
+bool timer_clear_state();
+
+int get_elapsed_time(const TimerState *state, char *buf, size_t len);
+
+#endif
